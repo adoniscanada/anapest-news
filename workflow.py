d4720aa915ab6aac6184a5cd899f359df69d9644
Adonis Canada <adonis.a.canada@gmail.com>
2025-05-26 08:39:12 -0400

    Added Logging

--- workflow.py
+++ workflow.py
@@ -1,7 +1,20 @@
-import json
+import logging
+import logging.handlers
 import datetime
 from pytz import timezone
 from cnn_scrape import generate_database
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "workflow.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter('%(asctime)s %(message)s')
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
 
 if __name__ == '__main__':
     tz = timezone('US/Eastern')
@@ -9,3 +22,4 @@ if __name__ == '__main__':
     db = generate_database(now.date())
     for i in db:
         print(db[i])
+    logger.info('Scraped ' + str(len(db)) + ' news articles.')
